7c5549ee066bdb61c7841fce557cc22936ee2bb0
mdsavian <marlon.savian@gmail.com>
2024-06-12 10:22:19 -0300

    feat: remove src folder

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-red-100">
+      Entrei aqui
+    </main>
+  );
+}
---
+++ pages/login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+  return <div className="flex bg-blue-100 h-24 w-24">Do login</div>;
+};
+
+export default Login;
