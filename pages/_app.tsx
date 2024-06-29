c4cb649fdd6e5d796260a6dc5cdaa9c52ac3ccd1
mdsavian <marlon.savian@gmail.com>
2024-06-29 18:23:25 -0300

    feat: add and configure axios

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import { AppProps } from "next/app";
 import "../app/globals.css";
+import axios from "axios";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const baseUrl = process.env.NEXT_PUBLIC_BUDGET_TRACKER_API || "";
+  axios.defaults.baseURL = baseUrl;
+  axios.defaults.withCredentials = true;
+  axios.defaults.headers.common["Content-Type"] = "application/json";
+
   return <Component {...pageProps} />;
 };
 
