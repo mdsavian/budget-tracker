1058262d089b3bbfea44b24c2b2b65108093f1eb
mdsavian <marlon.savian@gmail.com>
2024-07-30 21:07:04 -0300

    feat: fetch transaction by id

--- app/(dashboard)/dashboard/transaction/[transactionId]/page.tsx
+++ app/(dashboard)/dashboard/transaction/[transactionId]/page.tsx
@@ -1,12 +1,31 @@
+'use client';
 import { TransactionForm } from '@/components/forms/transaction-form';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import React from 'react';
+import axiosInstance from '@/lib/axios';
+import { Transaction } from '@/types';
+import { useParams } from 'next/navigation';
+import React, { useEffect } from 'react';
 
 export default function Page() {
+  const params = useParams();
+  const { transactionId } = params;
+
+  const [initialData, setInitialData] = React.useState<Transaction | null>(
+    null
+  );
+
+  useEffect(() => {
+    if (transactionId) {
+      axiosInstance.get(`/transaction/${transactionId}`).then((res) => {
+        setInitialData(res.data);
+      });
+    }
+  }, [transactionId]);
+
   return (
     <ScrollArea className="h-full">
       <div className="flex-1 space-y-4 p-5">
-        <TransactionForm initialData={null} key={null} />
+        <TransactionForm initialData={initialData} />
       </div>
     </ScrollArea>
   );
