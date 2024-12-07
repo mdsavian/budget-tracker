affbffd0aee3d1b9fad059a5d217346ed97947fd
Marlon Savian <marlon.savian@gmail.com>
2024-12-07 12:12:24 -0300

    refactor: extract transaction type select to component

---
+++ components/forms/TransactionForm/TransactionTypeSelect.tsx
@@ -0,0 +1,62 @@
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select';
+import { Control } from 'react-hook-form';
+import { TransactionFormValues } from './types';
+
+const TransactionTypeSelect = ({
+  loading,
+  control
+}: {
+  loading: boolean;
+  control: Control<TransactionFormValues>;
+}) => {
+  return (
+    <FormField
+      control={control}
+      name="transactionType"
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>Transaction Type</FormLabel>
+          <Select
+            disabled={loading}
+            onValueChange={field.onChange}
+            value={field.value}
+            defaultValue={field.value}
+          >
+            <FormControl>
+              <SelectTrigger>
+                <SelectValue
+                  defaultValue={field.value}
+                  placeholder="Transaction type"
+                />
+              </SelectTrigger>
+            </FormControl>
+            <SelectContent>
+              <SelectItem key="debit" value="Debit">
+                Debit
+              </SelectItem>
+              <SelectItem key="credit" value="Credit">
+                Credit
+              </SelectItem>
+            </SelectContent>
+          </Select>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default TransactionTypeSelect;
